dedebed24f77762eea7d3c5ed2739a90a4d60461
Cosmina.Dunca@arm.com <Cosmina.Dunca@arm.com>
2026-02-26 14:23:45 +0000

    Vectorise inverse hyperbolic special cases
    Vectorise SVE and AdvSIMD special-case handling for inverse
    hyperbolic functions (acosh, asinh, atanh).
    
    General-case improvements yield an average 11% speedup, with
    peak gains of up to 80%.
    
    For benchmarking I used Neoverse V2 with GCC@15.

--- sysdeps/aarch64/fpu/atanh_advsimd.c
+++ sysdeps/aarch64/fpu/atanh_advsimd.c
@@ -23,38 +23,43 @@
 const static struct data
 {
   struct v_log1p_data log1p_consts;
-  uint64x2_t one;
+  float64x2_t one;
   uint64x2_t sign_mask;
 } data = { .log1p_consts = V_LOG1P_CONSTANTS_TABLE,
-	   .one = V2 (0x3ff0000000000000),
+	   .one = V2 (1.0),
 	   .sign_mask = V2 (0x8000000000000000) };
 
 static float64x2_t VPCS_ATTR NOINLINE
-special_case (float64x2_t x, float64x2_t halfsign, float64x2_t y,
+special_case (float64x2_t ax, float64x2_t halfsign, float64x2_t y,
 	      uint64x2_t special, const struct data *d)
 {
   y = log1p_inline (y, &d->log1p_consts);
-  return v_call_f64 (atanh, vbslq_f64 (d->sign_mask, halfsign, x),
-		     vmulq_f64 (halfsign, y), special);
+  /* Copy the sign bit from the input to inf.  */
+  uint64x2_t is_one = vceqq_f64 (ax, d->one);
+  float64x2_t signed_inf
+      = vbslq_f64 (d->sign_mask, halfsign, v_f64 (INFINITY));
+  /* Here we check for all the rest of the cases where |x| > 1 will return a
+     NaN, including if x = NaN.  */
+  float64x2_t res = vbslq_f64 (special, v_f64 (NAN), vmulq_f64 (halfsign, y));
+
+  return vbslq_f64 (is_one, signed_inf, res);
 }
 
 /* Approximation for vector double-precision atanh(x) using modified log1p.
-   The greatest observed error is 3.31 ULP:
+   The greatest observed error is 2.81 + 0.5 ULP:
    _ZGVnN2v_atanh(0x1.ffae6288b601p-6) got 0x1.ffd8ff31b5019p-6
 				      want 0x1.ffd8ff31b501cp-6.  */
-VPCS_ATTR
-float64x2_t V_NAME_D1 (atanh) (float64x2_t x)
+VPCS_ATTR float64x2_t V_NAME_D1 (atanh) (float64x2_t x)
 {
   const struct data *d = ptr_barrier (&data);
 
   float64x2_t halfsign = vbslq_f64 (d->sign_mask, x, v_f64 (0.5));
   float64x2_t ax = vabsq_f64 (x);
-  uint64x2_t ia = vreinterpretq_u64_f64 (ax);
-  uint64x2_t special = vcgeq_u64 (ia, d->one);
+  uint64x2_t special = vcageq_f64 (x, d->one);
 
   float64x2_t y;
   y = vaddq_f64 (ax, ax);
-  y = vdivq_f64 (y, vsubq_f64 (vreinterpretq_f64_u64 (d->one), ax));
+  y = vdivq_f64 (y, vsubq_f64 (d->one, ax));
 
   if (__glibc_unlikely (v_any_u64 (special)))
     return special_case (ax, halfsign, y, special, d);
